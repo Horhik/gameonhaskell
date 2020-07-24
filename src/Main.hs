d754923c99eea2570089e92d1f857ae86df9f282
horhik <horhik@tuta.io>
2020-07-24 12:33:31 +0300

    added arrowkeys handlers

--- src/Main.hs
+++ src/Main.hs
@@ -2,41 +2,93 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Juicy
+import System.IO.Unsafe
+import Graphics.Gloss.Interface.IO.Interact
 
-data House  = House Picture Int Int
-data Player = Player Picture Int Int
 
-data Ground height= Ground height -- height is the y value where ground starts
-data Houses = [House]
-data Players = [Player]
 
-data World = World Players Houses Ground
+data House  = House Picture Float Float
+data Player = Player Picture Float Float
 
-loadImages :: IO (Picture, Picture)
+data Ground = Ground Int -- height is the y value where ground starts
+type Houses = [House]
+
+data World = World Player Houses Ground
+
+loadImages :: IO (Picture,  Picture)
 loadImages = do
   hero <- (loadJuicyPNG $ imagePath "hero.png")
-  house <- (loadJuicyPNG $ imagePath "house1.png")
-  return (hero, house)
+  house <- (loadJuicyPNG $ imagePath "home1.png")
+  return (fromMaybe hero, fromMaybe house)
+
+fromIO :: IO a -> a
+fromIO  = unsafePerformIO
+
+images :: (Picture, Picture)
+images = fromIO loadImages
+
+defaultPenguin :: Player
+defaultPenguin = Player img x y
+  where
+    img = ((translate x y) . (scale scaleXY scaleXY)) (fst images)
+    x = 0
+    y = (-40)
+    scaleXY = 2
 
 
-penguin = Player ()
+defaultHouse :: House
+defaultHouse = House img x y
+  where
+    img = ((translate x y) . (scale scaleXY scaleXY)) (snd images)
+    x = 200
+    y = 0
+    scaleXY = 5
 
-world []
 
+
+houses :: Houses
+houses = [defaultHouse]
+
+
+ground :: Ground
+ground = Ground 0
+
+fromMaybe :: Maybe Picture -> Picture
 fromMaybe (Just a) = a
+fromMaybe Nothing = Circle 30
 
 imagePath :: String -> String
 imagePath img = "/home/horhik/Pictures/Fun/" ++ img
 
-packPictures :: Picture -> Picture -> Picture
-packPictures pictures = Pictures pictures
 
-eventHandler :: Event -> world -> world
-eventHandler (EventKey KeyRight Down Modifiers (0,0) )
-game world = play FullScreen black 60 world (wold2picture world) (events world) wtf
+movePlayer :: Float -> Float -> Player -> Player
+movePlayer x y (Player sprite oldX oldY) = Player movedPicture newX newY
+  where
+    newX = x + oldX
+    newY = y + oldY
+    movedPicture = translate x y sprite
+
+wtf :: Float -> World -> World
+wtf iterations world = world
+
+newWorld = World defaultPenguin houses ground
+
+-- packPictures :: Picture -> Picture -> Picture
+-- packPictures pictures = Pictures pictures
+
+getHousePictures (House img _ _ ) = img
+
+world2picture :: World -> Picture
+world2picture (World (Player img1 _ _) imgs _ ) = Pictures $ img1 : (map getHousePictures imgs)
+
+eventHandler :: Event -> World -> World
+eventHandler (EventKey (SpecialKey KeyRight) _ _ _) (World player objs gr) = World (movePlayer 20 0 player) objs gr
+eventHandler (EventKey (SpecialKey KeyLeft) _ _ _) (World player objs gr) = World (movePlayer (-20) 0 player) objs gr
+eventHandler (EventKey (SpecialKey KeyUp) _ _ _) (World player objs gr) = World (movePlayer 0 20 player) objs gr
+eventHandler (EventKey (SpecialKey KeyDown) _ _ _) (World player objs gr) = World (movePlayer 0 (-20) player) objs gr
+eventHandler _ w = w
+
+game world  = play FullScreen cyan 20 world world2picture eventHandler wtf
 
 main :: IO ()
-main = do
-  png <- (loadJuicyPNG $ imagePath "home1.png")
-  let imgs = packPictures (fromMaybe png) (color red $ text "LMAO")
-  game imgs
+main = game newWorld
